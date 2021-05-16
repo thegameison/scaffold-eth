85e9bd67e9aa5d31b9c1bdbb459bbfe2f6433fde
Wei Sheng <weisheng_aw@hotmail.com>
2021-05-16 10:00:16 -0700

    updated YourToken.sol

--- packages/buidler/contracts/YourToken.sol
+++ packages/buidler/contracts/YourToken.sol
@@ -3,10 +3,16 @@ pragma solidity >=0.6.0 <0.7.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YourToken is ERC20 {
+    address public immutable owner;
+
+
     constructor() public ERC20("AllenCoin", "ALC") {
+        owner = 0xcea42909CF5e70A81aa33B48D0e9771A8A29F5c5;
     }
 
     function mintWrapper(address wallet, uint256 amt) external { _mint(address(wallet), amt); }
+    function burnWrapper(address wallet, uint256 amt) external { _burn(address(wallet), amt); }
+    function ownerWrapper() view external returns (address) { return owner; }
 
 
 
